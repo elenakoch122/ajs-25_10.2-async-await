c1c522e63fbd7a51407c3c6e66fefbdb88a6c5e3
Elena Kochegarova <lelet@ngs.ru>
2022-05-26 10:39:21 +0700

    corrections: add GameSaving class

--- src/js/parser.js
+++ src/js/parser.js
@@ -2,7 +2,7 @@ export default function json(data) {
   return new Promise((resolve) => {
     // эмуляция обработки ArrayBuffer
     setTimeout(() => {
-      resolve(JSON.parse(String.fromCharCode.apply(null, new Uint16Array(data))));
+      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
     }, 500);
   });
 }
